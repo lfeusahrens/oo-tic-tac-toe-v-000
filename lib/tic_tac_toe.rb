faf9e332a14c6df8c17a00a77944902472eeca5a
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-08-20 15:16:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,15 +57,10 @@ end
   end
 end 
  
- def turn_count(board)
-  turn = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-    turn += 1
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
   end
-  end
-  turn
-end
+  
    def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
