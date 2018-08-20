faec5d1edfce4917ad35a5c3b1d41c83d6a70fd4
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-08-20 15:51:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,18 +43,6 @@ WIN_COMBINATIONS = [
     position_taken?(index) && index.between?(0, 8)
   end
 
-  def turn
-    puts "Please enter 1-9:"
-    user_input = gets.strip
-    index = input_to_index(user_input)
-  
-    if valid_move?(index)
-      move(index, current_player)
-      display_board
-    else
-      turn
-    end
-  end 
  
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
