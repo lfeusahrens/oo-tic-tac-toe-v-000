a7e508ab1c0bda57a9142086db0703ac96bcbdec
Lauren Feusahrens <lrfeusahrens@gmail.com>
2018-08-20 15:47:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ WIN_COMBINATIONS = [
   end 
  
   def valid_move?(index)
-    @board.!position_taken?(index) && index.between?(0, 8)
+    @board.position_taken?(index) && index.between?(0, 8)
   end
 
   def turn
